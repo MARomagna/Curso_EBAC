10979376070d8022760221a50255c5d5297caa8c
Marco Romagna <100988817+MARomagna@users.noreply.github.com>
2023-03-30 16:12:18 -0300

    Inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("header button").click(function () {
+    $("form").slideDown();
+    $("#botao-cancelar").click(function () {
+      $("form").slideUp();
+    });
+  });
+});
+$("form").on("submit", function (e) {
+  e.preventDefault();
+  const novaTarefa = $("#nova-tarefa").val();
+  const novoItem = $(`<li style="display:none"></li>`);
+  $(`<li>"${novaTarefa}"</li>`).appendTo(novoItem);
+  $(novoItem).appendTo("ul");
+  $(novoItem).fadeIn(1000);
+  $("#nova-tarefa").val("");
+});
