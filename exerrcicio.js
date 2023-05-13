e0b45bcb0a86031678cf0e567ee2f1fd2c25bdbc
Marco Romagna <100988817+MARomagna@users.noreply.github.com>
2023-05-13 17:21:58 -0300

    inicial

---
+++ exerrcicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicar = (base, altura) => base * altura;
+console.log(multiplicar(5, 8));
+const saudacao = (nome) => `Ola ${nome}`;
+console.log(saudacao("Marco"));
---
+++ exerrcicio.ts
@@ -0,0 +1,5 @@
+const multiplicar = (base: number, altura: number): number => base * altura;
+console.log(multiplicar(5, 8));
+
+const saudacao = (nome: string): string => `Ola ${nome}`;
+console.log(saudacao("Marco"));
